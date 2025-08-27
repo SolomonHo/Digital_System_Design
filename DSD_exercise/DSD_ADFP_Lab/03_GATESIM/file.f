cabce313ef08a097a0f47a269807fb6848470ab8
SolomonHo <solomonho@ntu.edu/tw>
2025-08-27 14:27:39 +0800

    remove .zip

--- DSD_exercise/DSD_ADFP_Lab/03_GATESIM/file.f
+++
@@ -1,5 +0,0 @@
-../00_TESTBED/TESTBED.v
-../00_TESTBED/PATTERN.v
-../02_SYN/Netlist/ADDER_syn.v
-
--v /share1/tech/ADFP/Executable_Package/Collaterals/IP/stdcell/N16ADFP_StdCell/VERILOG/N16ADFP_StdCell.v
